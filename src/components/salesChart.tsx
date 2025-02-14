5992556bfa972c002aba1a6c453d0a9973c2c268
jospin6 <jospinndagano1@gmail.com>
2025-02-14 15:51:45 +0200

    add current user in sales chart

--- src/components/salesChart.tsx
+++ src/components/salesChart.tsx
@@ -4,15 +4,18 @@ import { fetchSalesLast7Days } from "@/features/saleSlice";
 import { Line } from "react-chartjs-2";
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from "chart.js";
 import { AppDispatch, RootState } from "@/features/store";
+import { currentUser } from "@/features/authSlice";
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 const SalesChart = () => {
   const dispatch = useDispatch<AppDispatch>();
   const salesLast7Days = useSelector((state: RootState) => state.sale.salesLast7Days);
+  const getCurrentUser = useSelector((state: RootState) => state.auth.user)
 
   useEffect(() => {
     dispatch(fetchSalesLast7Days());
+    dispatch(currentUser())
   }, [dispatch]);
 
   const data = {
